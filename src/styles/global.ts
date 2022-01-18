dbec74c3094af260bfbb2db36d1857864f216f40
Sandro Carvalho do Nascimento <sandro.cdonascimento@hotmail.com>
2022-01-18 12:18:08 -0300

    #1 - Estrutura a Apliacão

---
+++ src/styles/global.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+:root{
+  --background: #f0f2f5;
+  --red: #e52e4d;
+  --blue: #5429cc;
+  
+  --blue-light: #6933ff;
+  
+  --text-title: #363f5f;
+  --tex-body: #969cb3;
+  
+  --shape: #FFFFFF;
+}
+
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+
+//font padrao 16px
+
+html {
+  @media (max-width:1080px) {
+    font-size:93.75% //15px
+  }
+
+  @media (max-width:720) {
+    font-size:87.5% //14px
+  }
+}
+
+//REM MEDIDA CSS 1 REM = A 16PX 
+
+body {
+  background: var(--background);
+  -webkit-font-smoothing: antialiased;
+}
+
+body,input,textarea,button {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+}
+
+h1,h2,h3,h4,h5,h6,strong {
+  font-weight: 600;
+}
+
+button {
+  cursor: pointer;
+}
+
+[disabled] {
+  opacity:0.6;
+  cursor: not-allowed;
+}
+`
